0dc1d8e48e89df5089e2c8965da3b97c8f17e7ef
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-09-20 22:50:26 -0500

    Introduccion JavaScript

--- 20_09_09-React/20_09_14-02-Intro-JavaScript/src/bases/08-imp-exp.js
+++ 20_09_09-React/20_09_14-02-Intro-JavaScript/src/bases/08-imp-exp.js
@@ -1,13 +1,15 @@
 // import {heroes} from './data/heroes'
 // import {heroes} from './data/heroes'
-import heroes, {owners} from '../data/heroes';
+// import heroes, {owners} from '../data/heroes';
+import heroes from '../data/heroes';
 
-console.log(owners);
+// console.log(owners);
 
-const getHeroeById = (id) => heroes.find((heroes) => heroes.id === id); 
+export const getHeroeById = (id) => heroes.find((heroes) => heroes.id === id); 
 
-console.log(getHeroeById(2));
+// console.log(getHeroeById(2));
 
 // find?, filter
-const getHeroeByOwner = (owner) => heroes.filter((heroes) => heroes.owner === owner); 
-console.log(getHeroeByOwner('Marvel'));
+export const getHeroeByOwner = (owner) => heroes.filter((heroes) => heroes.owner === owner); 
+
+// console.log(getHeroeByOwner('Marvel'));
