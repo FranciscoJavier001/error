adf7ca4477373f61d4954c90cd50e1f3b87c6afe
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-09-21 22:29:44 -0500

    Introduccion JavaScript

---
+++ 20_09_09-React/20_09_14-02-Intro-JavaScript/src/index.js
@@ -0,0 +1,22 @@
+import  { getHeroeById } from './bases/08-imp-exp';
+
+const getHeroeByIdAsync = (id) => {
+
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            // Tarea
+            // importen el
+            const p1 = getHeroeById(id);
+            if(p1){
+                resolve(p1);
+            } else {
+                reject('No se pudo encontrar el heroe')
+            }
+        }, 2)
+    });
+}
+
+getHeroeByIdAsync(10)
+    .then(console.log)
+    .catch(console.warn);
