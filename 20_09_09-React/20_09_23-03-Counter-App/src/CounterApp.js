60b7c5bbdef5e2087cb189122ce261b05d4ca412
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-09-25 15:52:38 -0500

    Eventos

--- 20_09_09-React/20_09_23-03-Counter-App/src/CounterApp.js
+++ 20_09_09-React/20_09_23-03-Counter-App/src/CounterApp.js
@@ -3,10 +3,17 @@ import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
 
+    // handleAdd
+    const handleApp=(e)=>{
+        console.log(e);
+    }
+
     return (
         <>
         <h1>CounterApp</h1>
         <h2>{value}</h2>
+
+        <button onClick={handleApp}>+1</button>
         </>
     );
 }
