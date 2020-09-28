6ac94a5c70e707e1d6bb0b1dd148622dec100d17
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-09-28 17:58:48 -0500

    Pruebas

--- 20_09_09-React/20_09_23-03-Counter-App/src/tests/demo.test.js
+++ 20_09_09-React/20_09_23-03-Counter-App/src/tests/demo.test.js
@@ -1,9 +1,14 @@
-
-
-test('debe de ser true ', () => {
-    const isActive = true;
-
-    if(isActive){
-        throw new Error('No esta activo')
-    }
-})
+describe('Pruebas en el archivo demo.test.js', () => {
+    
+    test('deben de ser iguales los string ', () => {
+       
+        // 1. Inicializacion
+        const mensaje = 'Hola Mundo';
+    
+        // 2. Estimulo
+        const mensaje2 = `Hola Mundo`;
+    
+        // 3. Observar el comportamiento
+        expect(mensaje).toBe(mensaje2); // ===
+    })
+});
