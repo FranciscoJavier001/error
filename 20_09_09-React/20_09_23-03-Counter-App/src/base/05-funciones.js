3c3736e40a27f5237b7d8e04f839edbd31ef9f23
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-09-30 13:08:12 -0500

    Falla en funciones

--- 20_09_09-React/20_09_23-03-Counter-App/src/base/05-funciones.js
+++ 20_09_09-React/20_09_23-03-Counter-App/src/base/05-funciones.js
@@ -1,7 +1,4 @@
-// Funciones en JS
-// const saludar = function (nombre){
-//     return `Hola, ${nombre}`;
-// }
+
 
 const saludar2 = (nombre) =>{
     return `Hola, ${nombre}`
@@ -10,41 +7,13 @@ const saludar2 = (nombre) =>{
 const saludar3 = (nombre) => `Hola, ${nombre}`;
 const saludar4 = () => `Hola Mundo`;
 
-// console.log(saludar('Goku'));
-
-console.log(saludar2('Vegeta'));
-console.log(saludar3('Goku'));
-console.log(saludar4());
-
-const getUser = () => ({
+export const getUser = () => ({
         uid: 'ABD123',
-        username: 'El_Papi102'
-    })
-
-const user = getUser()
-console.log(user);
-
-// Tarea
-// 1. Transformen a una funcion de Flecha
-// 2. Tiene que retornar un objeto implicito
-// 3. Pruebas
+        username: 'El_Papi1502'
+    });
 
 
-// function getUsuarioActivo(nombre){
-//     return{
-//         uid: 'ABC567',
-//         username: nombre
-//     }
-// };
-
-// const usuarioActivo = getUsuarioActivo('Francisco');
-// console.log(usuarioActivo);
-
-
-const getUsuarioActivo = (nombre) => ({
+export const getUsuarioActivo = (nombre) => ({
     uid: 'ABC678',
     username: nombre
-});
-
-const usuarioActivo = getUsuarioActivo('Francisco');
-console.log(usuarioActivo);
+})
---
+++ 20_09_09-React/20_09_23-03-Counter-App/src/tests/base/05-funciones.test.js
@@ -0,0 +1,47 @@
+import '@testing-library/jest-dom';
+
+import { getUser, getUsuarioActivo } from '../../base/05-funciones'
+
+describe('Pruebas en 05-funciones', () => {
+
+    // test('getUser debe de retornar un objeto ', () => {
+
+    //     const userTest = {
+    //     uid: 'ABD123',
+    //     username: 'El_Papi1502'
+    //     }
+
+    //     const user = getUser();
+
+    //     expect(user).toEqual(userTest);
+    // })
+
+// getUsuarioActio debe de retornar un objeto
+// getusuario activo debe retornar un objeto
+
+// evaloar lo que se retorna en "username:nobre", con un objeto que vamos a definir en la otra pagina, getuseractivo debe retornar un nombre en el username:
+
+// test('getUser debe de retornar el nombre ', () => {
+
+//     uid: 'ABD123',
+//     username: (nombre);
+//     }
+
+//     const user = getUser(nombre);
+
+//     expect(user).toEqual(userTest);
+// })
+
+/**  */
+/**  */
+// RESPUESTA
+test('getUser debe de retornar un objeto  ', () => {
+    
+    const nombre = 'Juan',
+    const user = getUsuarioActivo(nombre)
+
+    console.log(object);
+})
+})
+/**  */
+/**  */
