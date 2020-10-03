3a5dfb035d9711ff199d0d775f71e082e15f9e0e
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-10-02 22:12:40 -0500

    Pruebas Asyncronas

---
+++ 20_09_09-React/20_09_23-03-Counter-App/src/tests/base/09-promesas.test.js
@@ -0,0 +1,25 @@
+const { getHeroeByIdAsync } = require("../../base/09-promesas");
+const { default: heroes } = require("../../data/heroes");
+
+describe('Pruebas con promesas', () => {
+    test('getHeroeByIdAsync debe de retornar un Heroe Async ', (done) => {
+        const id = 1;
+
+        getHeroeByIdAsync(id)
+        .then(heroe => {
+            expect(heroe).toBe(heroes[0]);
+            done();
+        });
+    });
+
+    test('debe de obtener un error si el heroe por id no existe ', (done) => {
+        const id = 10;
+        getHeroeByIdAsync(id)
+        .catch(error => {
+            expect(error).toBe('no se pudo encontrar el heroe!!!');
+            done();
+        });
+    });
+    
+    
+})
