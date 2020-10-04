1a1c3fc0006781da514bf160dda70b407d41a648
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-10-04 16:03:02 -0500

    Enzyme Testing Unit

--- 20_09_09-React/20_09_23-03-Counter-App/src/tests/PrimeraApp.test.js
+++ 20_09_09-React/20_09_23-03-Counter-App/src/tests/PrimeraApp.test.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import {shallow} from 'enzyme';
+
 import PrimeraApp from '../PrimeraApp';
 
 describe('Pruebas en <PrimeraApp />', () => {
-    test('debe de mostrar el mensaje "Hola, Soy Francisco ', () => {
-        
-        const saludo = 'Hola, Soy Francisco';
-
-        const {getByText} = render(<PrimeraApp saludo={saludo}/>);
-
-        expect(getByText(saludo)).toBeInTheDocument();
-    })
     
+    // test('debe de mostrar el mensaje "Hola, Soy Francisco ', () => {
+    //     const saludo = 'Hola, Soy Francisco';
+    //     const {getByText} = render(<PrimeraApp saludo={saludo}/>);
+    //     expect(getByText(saludo)).toBeInTheDocument();
+    // })
+    
+    test('debe de mostrar <PrimeraApp />> correctamente', () => {
+
+        const saludo = 'Hola, Soy Francisco';
+        const wrapper = shallow(<PrimeraApp saludo={saludo}/>) 
+
+       expect(wrapper).toMatchSnapshot();
+    });
 })
