408d766f048981f9ee59f7099d311f1d23724418
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-10-09 19:04:13 -0500

    GifExpertApp

--- 20_09_09-React/20_10_09-04-Gif-Expert-App/src/GifExpertApp.js
+++ 20_09_09-React/20_10_09-04-Gif-Expert-App/src/GifExpertApp.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball']
+    const [categories, setcategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball'])
+
+    const handleAdd = () => {
+        // setcategories(['HunterXHunter', ...categories,]);
+        setcategories(cats => [...cats, 'HunterXHunter']); /** El UseState me dio la habilidad de hacerlo directamente con el setcategories */
+    }
+
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr/>
+
+            <button onClick={handleAdd}>Agregar</button>
+
+            <ol>
+                {
+                    categories.map(category => { /** El Map barre el arreglo */
+                        return <li key={category}>{category}</li>
+                    })
+                }
+            </ol>
+
         </>
     )
 }
