f3727a80256a8e2056713e776211c0d292fc68a9
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-10-12 19:56:57 -0500

    useEffect

--- 20_09_09-React/20_10_09-04-Gif-Expert-App/src/components/GifGrid.js
+++ 20_09_09-React/20_10_09-04-Gif-Expert-App/src/components/GifGrid.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const GifGrid = ({category}) => {
+
+    const [count, setCount] = useState(0);
+
+    useEffect(()=> {
+        getGifs();
+    }, [])
 
     const getGifs = async () => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+morty&limit=10&api_key=tfRgcwv7vxAyL3vMQapLiD0NQw3148nH';
@@ -16,13 +22,16 @@ export const GifGrid = ({category}) => {
         })
 
         console.log(gifs);
+        // setImgs
     }
 
-    getGifs();
+    // getGifs();
 
     return (
         <>
             <h3>{category}</h3>
+            <h3>{count}</h3>
+            <button onClick={()=> setCount(count+1)}></button>
         </>
     )
 }
