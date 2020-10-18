9f52496412c5a5cb5675b2a654e5c5c2f04cfe50
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-10-17 21:58:17 -0500

    Custom Hooks

--- 20_09_09-React/20_10_09-04-Gif-Expert-App/src/components/GifGrid.js
+++ 20_09_09-React/20_10_09-04-Gif-Expert-App/src/components/GifGrid.js
@@ -1,23 +1,19 @@
 import React  from 'react'
 import { useFetchGifs } from '../hooks/useFetchGifs'
-// import { GifGridItem } from './GifGridItem';
-// import { getGifs } from '../helpers/getGifs';
+import { GifGridItem } from './GifGridItem';
+
 
 export const GifGrid = ({ category }) => {
 
-    // const [images, setImages] = useState([]);
-    const {loading} = useFetchGifs();
-
-    // useEffect(() => {
-    //     getGifs(category)
-    //     .then(imgs => setImages(imgs))
-    // }, [category])
+    const {data:images, loading} = useFetchGifs(category);
 
     return (
         <>
             <h3>{category}</h3>
-            {loading ? 'Cargando...': 'Data cargada'}
-            {/* <div className="card-grid">
+
+            {loading && <p>Loading</p>}
+
+            <div className="card-grid">
                 {
                     images.map(img => (
                         <GifGridItem
@@ -26,7 +22,7 @@ export const GifGrid = ({ category }) => {
                         />
                     ))
                 }
-            </div> */}
+            </div>
         </>
     )
 }
