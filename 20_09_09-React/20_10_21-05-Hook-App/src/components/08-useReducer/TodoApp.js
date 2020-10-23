25d6f9647f5fe42f8ede7110be744eba78d8f6ec
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-10-22 20:40:13 -0500

    Anotaciones

--- 20_09_09-React/20_10_21-05-Hook-App/src/components/08-useReducer/TodoApp.js
+++ 20_09_09-React/20_10_21-05-Hook-App/src/components/08-useReducer/TodoApp.js
@@ -6,79 +6,64 @@ import { TodoAdd } from './TodoAdd';
 import './styles.css';
 
 const init = () => {
-
     return JSON.parse(localStorage.getItem('todos')) || [];
-
 }
 
+// Esto se importa desde todoReducer.js, estas son las acciones que viene en el Case
 export const TodoApp = () => {
 
-    const [ todos, dispatch ] = useReducer(todoReducer, [], init);
+    const [ todos, dispatch ] = useReducer(todoReducer, [], init); /** Aqui sabe a que reducer hay que mandarle la informacion y cuando cambia el State va a redibujar lo que cambie, entonces al Dispatch hay que mandarle la accion y es el segundo argumento */
     
     useEffect( ()=> {
         localStorage.setItem('todos', JSON.stringify( todos ) );
     }, [todos]);
 
-
+    // EStas son las acciones que viene en el case
     const handleDelete = ( todoId ) => {
-
         const action = {
             type: 'delete',
-            payload: todoId
+            payload: todoId /** Esta es el nuevo estado despues de lo que se hace */
         }
-
-        dispatch( action );
+        dispatch( action ); /** Aqui mandamos la nueva accion */
     }
 
     const handleToggle = ( todoId ) =>{
-        
         dispatch({
             type: 'toggle',
             payload: todoId
         });
-
     }
 
     const handleAddTodo = ( newTodo ) => {
-        
         dispatch({
             type: 'add',
             payload: newTodo
         });
-
     }
 
-
-    return (
+    // Aqui retornamos el numero de arrelos del Todo
+    return ( 
         <div>
-            <h1>TodoApp ( { todos.length } ) </h1>
+            <h1>TodoApp ( { todos.length } ) </h1> 
             <hr />
 
             <div className="row">
-
                 <div className="col-7">
-
                     <TodoList 
                         todos={ todos }
                         handleDelete={ handleDelete }
                         handleToggle={ handleToggle }
                     />
-
                 </div>
 
-
+                {/* Aqui es donde se muestra lo que se va a agregar, es el input y el boton */}
                 <div className="col-5">
-                    
                     <TodoAdd 
                         handleAddTodo={ handleAddTodo }
                     />
-                    
-
                 </div>
-
-
+                
             </div>
-
         </div>
     )
 }
--- 20_09_09-React/20_10_21-05-Hook-App/src/components/08-useReducer/todoReducer.js
+++ 20_09_09-React/20_10_21-05-Hook-App/src/components/08-useReducer/todoReducer.js
@@ -1,10 +1,12 @@
 
-
+// Este es el que importa las acciones
 export const todoReducer = ( state = [], action ) => {
 
-    switch ( action.type ) {
+    // Aqui mostramos las acciones que viene directamente del TodoApp.js
+    // Crear las acciones y mandarselas al reducer, el reducer las ejecuta, me regresa un nuevo estado y como estoy trabajando en base a mi useReducer este se va a encargar de redibujar lo que hay que redibujar 
+    switch ( action.type ) { /** Wey, esto ya es el boton, con toda la logica para que agrege una nueva tarea */
         case 'add':
-            return [ ...state, action.payload ];
+            return [ ...state, action.payload ]; /** Como nos damos cuenta, aqui se va a agregar, y la ltima va a ser la primera, manteniendo la fotografia como estaba antes*/
 
         case 'delete':
             return state.filter( todo => todo.id !== action.payload ); // 123123123
@@ -32,7 +34,7 @@ export const todoReducer = ( state = [], action ) => {
             })
 
         default:
-            return state;
+            return state; /** Si vemos que no hay cambio manda el mismo estado */
     }
 
 
