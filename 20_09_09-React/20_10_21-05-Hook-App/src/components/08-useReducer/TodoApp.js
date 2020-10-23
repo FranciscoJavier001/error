614cb0645a58dfc1a46de579cc7d39a66e97afde
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-10-22 21:02:18 -0500

    Anotaciones

--- 20_09_09-React/20_10_21-05-Hook-App/src/components/08-useReducer/TodoApp.js
+++ 20_09_09-React/20_10_21-05-Hook-App/src/components/08-useReducer/TodoApp.js
@@ -5,17 +5,20 @@ import { TodoList } from './TodoList';
 import { TodoAdd } from './TodoAdd';
 import './styles.css';
 
+//** Cuando se recarga este componente se vuelve a iniciar esta funcion del init la cual lee el localstorage y me ayuda a establecer el estado inicial del reducer */
 const init = () => {
-    return JSON.parse(localStorage.getItem('todos')) || [];
+    return JSON.parse(localStorage.getItem('todos')) || []; //** Esto permite que se lean los todos, y se recupera con el JSON.parse y este se va a regresar como un arreglo o si no hay nada se regresa un arreglo vacio que ese es el inicial, y si esta null entonces retorna un arreflo vacio */
 }
 
 // Esto se importa desde todoReducer.js, estas son las acciones que viene en el Case
 export const TodoApp = () => {
 
     const [ todos, dispatch ] = useReducer(todoReducer, [], init); /** Aqui sabe a que reducer hay que mandarle la informacion y cuando cambia el State va a redibujar lo que cambie, entonces al Dispatch hay que mandarle la accion y es el segundo argumento */
+    // Init va a computar el estado inicial para que esta funcion no se este ejecutando y ejecutando cada vez que hay cambios, esta es una funcion comun y corriente y va a regresar el arreglo vacio, y lo que sea que retorne es lo que va a mostrar el initialState
     
+    // Va a disparar un callback y la dependencia que va a tener van a ser los Todos, si los Todos cambian eso significa que vamos a volver a guardar en el localStorage y eso es lo que hace el useEffect 
     useEffect( ()=> {
-        localStorage.setItem('todos', JSON.stringify( todos ) );
+        localStorage.setItem('todos', JSON.stringify( todos ) ); /** Asi se guarda en el localStorage - Este va a guadar en el LocalStorage cualquier cambio que pase en los todos de la constante de arriba */ 
     }, [todos]);
 
     // EStas son las acciones que viene en el case
--- 20_09_09-React/20_10_21-05-Hook-App/src/hooks/useForm.js
+++ 20_09_09-React/20_10_21-05-Hook-App/src/hooks/useForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 
-export const useForm = ( initialState = {} ) => {
+export const useForm = ( initialState = {} ) => { /** useForm que recibe un objeto en el cual el objeto tiene las propiedades qu cada una va a ser un campo de texto o un selector o un campo en el formulario que quiero manipular */
     
     const [values, setValues] = useState(initialState);
 
@@ -10,7 +10,7 @@ export const useForm = ( initialState = {} ) => {
     }
 
 
-    const handleInputChange = ({ target }) => {
+    const handleInputChange = ({ target }) => { /** El handleInputChange me va a permitir para poderlo leer rapidamente */
 
         setValues({
             ...values,
@@ -19,6 +19,6 @@ export const useForm = ( initialState = {} ) => {
 
     }
 
-    return [ values, handleInputChange, reset ];
+    return [ values, handleInputChange, reset ]; /** Lo regresa como un arreglo, el primer valor es el estado del formulario y el segundo es el handleInputChange para cambiar los valores del formulario */
 
 }
