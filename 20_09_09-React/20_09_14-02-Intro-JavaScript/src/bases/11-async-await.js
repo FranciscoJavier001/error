de4a8779e1081b37cd152c0acc9edc62409af870
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-11-09 13:44:23 -0600

    Anotaciones

--- 20_09_09-React/20_09_14-02-Intro-JavaScript/src/bases/11-async-await.js
+++ 20_09_09-React/20_09_14-02-Intro-JavaScript/src/bases/11-async-await.js
@@ -1,19 +1,18 @@
-/** Wow, encontre como crear estoy que a toda madre, neta necesitaba saber que ra estoy mas que eso como hacerlo, la verdad me di cuenta que mi voluntad es poderosa y que si algo  rdta en mi fuerza de voluntad lo voy a lograr */
-/**  */
-/** pues ya con eso esta chido */
-
-
 // Este wey queria que pusieramos la imgen, es decir que hagamos otra peticion de response y que me tajeras una imagen,
 // segun yo lo que falta es una madre que se llama data y a esa madre pasarle una url, despues que lance una madre que me cree un elemento en el DOM
 // y que sea una img.src = url;
 // y que ya aparezca
 
+//** Async se usa para que algo te retorne una promesa */
+//** Await nos dice, espera que esta promesa termine, antes de ejecutar lo siguiente */
+
+//** Puedes usar el async, pero no puedes utilizar el await sin el async */
 const getImagen = async() => { //Esta es una peticion async que pide que me respondan una variale y que esa variable ponerla en el DOM y que ahi se quede, osea que apareza una imagen 
 
     try{
         const apiKey = 'tfRgcwv7vxAyL3vMQapLiD0NQw3148nH'; //Creo apiKey
     const resp = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
-    const {data} = await resp.json(); //Aqui hizo la desestructuracion de objetos, porque estaba bien cabron todo lo que llevaba
+    const {data} = await resp.json(); //** El Objeto JSON regresa una promesa y debo de esperarme a que se resuelta la promesa para obtener la data */
 
     const {url} = data.images.original; /**Como este wey ya habiamos obtenido la ruta a traves de desesructurar el objeto data */
 
--- 20_09_09-React/20_09_14-02-Intro-JavaScript/src/bases/12-ternario.js
+++ 20_09_09-React/20_09_14-02-Intro-JavaScript/src/bases/12-ternario.js
@@ -10,8 +10,8 @@ const activo = true;
  * }
  */
 
-// const mensaje = (activo) ? 'Activo' : 'Inactivo';
+// const mensaje = (activo) ? 'Activo' : 'Inactivo'; //** Cuando se cunple ponemos el ? en caso que no se cumpla ponemos  */
 // const mensaje = (activo) ? 'Activo' : null;
 const mensaje = (activo === true) && 'Activo'
 
-console.log(mensaje);
+console.log(mensaje);QAZWSXEDC4rfv
