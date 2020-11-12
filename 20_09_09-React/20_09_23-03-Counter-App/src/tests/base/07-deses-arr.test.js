5d3ec9e6cdda1457c692b8637383468830d7ce6c
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-11-11 19:48:11 -0600

    Anotaciones

--- 20_09_09-React/20_09_23-03-Counter-App/src/tests/base/07-deses-arr.test.js
+++ 20_09_09-React/20_09_23-03-Counter-App/src/tests/base/07-deses-arr.test.js
@@ -3,14 +3,14 @@ import { retornaArreglo } from '../../base/07-deses-arr';
 describe('Pruebas en desestructuracion ', () => {
     test('debe de retornas un string y un numero ', () => {
         
-        const [letras, numeros] = retornaArreglo() //['ABC', 123];
+        const [letras, numeros] = retornaArreglo() //['ABC', 123]; //** Podemos hacer desestructuraciones de funciones que vengan desde otro lado */
 
         // console.log(typeof numeros);
 
-        expect(letras).toBe('ABC');
+        expect(letras).toBe('ABC'); //** Hacemos la comparacion que queramos */
         expect(typeof letras).toBe('string');
 
-        expect(numeros).toBe(123);
+        expect(numeros).toBe(123); //** Hacemos la comparacion que queramos */
         expect(typeof numeros).toBe('number');
     })
     
