3df6109dd7729522a3d57192ee7fd3beb771ac6d
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-11-11 19:09:55 -0600

    Anotaciones

--- 20_09_09-React/20_09_23-03-Counter-App/src/tests/base/05-funciones.test.js
+++ 20_09_09-React/20_09_23-03-Counter-App/src/tests/base/05-funciones.test.js
@@ -6,7 +6,7 @@ describe('Pruebas en 05-funciones', () => { //** Descripcion de la prueba */
 
     test('getUser debe de retornar un objeto  ', () => { //** Lo que se va a probar */
 
-        const userTest = {
+        const userTest = { //** Esta funcion retorna un objeto */
          uid: 'ABC123',
          username: 'El_Papi1502'
     }
@@ -20,8 +20,9 @@ describe('Pruebas en 05-funciones', () => { //** Descripcion de la prueba */
     test('getUsuarioActivo debe de retorna un objeto', () => {
         const nombre = 'Juan';
         const user = getUsuarioActivo(nombre);
+        //** getUsuarioActivo = uid: 'ABC567', username: nombre */
 
-        expect(user).toEqual({
+        expect(user).toEqual({ //** Este operador es de Debe ser Equal, osea mas que igual */
             uid: 'ABC567',
             username: nombre
         });
